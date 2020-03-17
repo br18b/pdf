ea1ad8f699b92d666f03a77442b19e19c825be9b
brano <br18b@my.fsu.edu>
2020-03-17 14:08:45 -0400

    loads all required fields into the dataset

--- load_operators.py
+++ load_operators.py
@@ -28,14 +28,51 @@ def dvzdy(field, data):
 def dvzdz(field, data):
 	return operators.d(data, 'z-velocity', 2)
 
+def absv(field, data):
+	return (data['x-velocity']**2 + data['y-velocity']**2 + data['z-velocity']**2)**0.5
+
 def absgradrho(field, data):
 	return (data['drhodx']**2 + data['drhody']**2 + data['drhodz']**2)**0.5
 
 def vdotgradrho(field, data):
 	return data['x-velocity']*data['drhodx'] + data['y-velocity']*data['drhody'] + data['z-velocity']*data['drhodz']
 
+def vdotgradrhocos(field, data):
+	return data['vdotgradrho']/(data['absgradrho']*data['absv'])
+
+def vdotgradrhoangle(field, data):
+	out = numpy.zeros_like(data['vdotgradrhocos'])
+	out[:] = numpy.arccos(data['vdotgradrhocos'])
+	return out
+
 def divv(field, data):
 	return data['dvxdx'] + data['dvydy'] + data['dvzdz']
+
+def vdotgradvx(field, data):
+	return data['x-velocity']*data['dvxdx'] + data['y-velocity']*data['dvxdy'] + data['z-velocity']*data['dvxdz']
+
+def vdotgradvy(field, data):
+	return data['x-velocity']*data['dvydx'] + data['y-velocity']*data['dvydy'] + data['z-velocity']*data['dvydz']
+
+def vdotgradvz(field, data):
+	return data['x-velocity']*data['dvzdx'] + data['y-velocity']*data['dvzdy'] + data['z-velocity']*data['dvzdz']
+
+def absvdotgradv(field, data):
+	return (data['vdotgradvx']**2 + data['vdotgradvy']**2 + data['vdotgradvz']**2)**0.5
+
+def vdotvdotgradv(field, data):
+	return data['x-velocity']*data['vdotgradvx'] + data['y-velocity']*data['vdotgradvy'] + data['z-velocity']*data['vdotgradvz']
+
+def rhodivv(field, data):
+	return data['Density']*data['divv']
+
+def vdotvdotgradvcos(field, data):
+	return data['vdotvdotgradv']/(data['absv']*data['absvdotgradv'])
+
+def vdotvdotgradvangle(field, data):
+	out = numpy.zeros_like(data['vdotvdotgradvcos'])
+	out[:] = numpy.arccos(data['vdotvdotgradvcos'])
+	return out
 
 density_validators = [yt.ValidateSpatial(1,['density'])]
 
@@ -43,105 +80,89 @@ vx_validators = [yt.ValidateSpatial(1,['x-velocity'])]
 vy_validators = [yt.ValidateSpatial(1,['y-velocity'])]
 vz_validators = [yt.ValidateSpatial(1,['z-velocity'])]
 
-density_dx_requirement_set = {"drhodx", "|gradrho|", "vdotgradrho", "vdotgradrhoangle"}
-density_dy_requirement_set = {"drhody", "|gradrho|", "vdotgradrho", "vdotgradrhoangle"}
-density_dz_requirement_set = {"drhodz", "|gradrho|", "vdotgradrho", "vdotgradrhoangle"}
-
-dvxdx_requirement_set = {"dvxdx", "vdotgradv", "divv"}
-dvxdy_requirement_set = {"dvxdy", "vdotgradv"}
-dvxdz_requirement_set = {"dvxdz", "vdotgradv"}
-
-dvydx_requirement_set = {"dvydx", "vdotgradv"}
-dvydy_requirement_set = {"dvydy", "vdotgradv", "divv"}
-dvydz_requirement_set = {"dvydz", "vdotgradv"}
-
-dvzdx_requirement_set = {"dvzdx", "vdotgradv"}
-dvzdy_requirement_set = {"dvzdy", "vdotgradv"}
-dvzdz_requirement_set = {"dvzdz", "vdotgradv", "divv"}
-
 drho_units = "code_mass/code_length**4"
 
 dv_units = "1/code_time"
 
-taskdictfun = {"|gradrho|": absgradrho, "absgradrho": absgradrho, "vdotgradrho": vdotgradrho, "divv": divv}
-taskdictstr = {"vx": "x-velocity", "vy": "y-velocity", "vz": "z-velocity",
-"drhodx": "drhodx", "drhody": "drhody","drhodz": "drhodz",
-"|gradrho|": "absgradrho", "vdotgradrho": "vdotgradrho", "divv": "divv"}
-taskdictuni = {"|gradrho|": "code_mass/code_length**4", "vdotgradrho": "code_mass/(code_length**3*code_time)",
-"divv": "1/code_time"}
-#taskdictval = {"|gradrho|": [yt.ValidateGridType()], "vdotgradrho": [yt.ValidateGridType()]}
+a_units = "code_length/code_time**2"
 
+possible_fields = ["drhodx", "drhody", "drhodz", "absv", "dvxdx", "dvxdy", "dvxdz",
+"dvydx", "dvydy", "dvydz", "dvzdx", "dvzdy", "dvzdz", "divv", "absgradrho", "vdotgradrho",
+"vdotgradrhocos", "vdotgradrhoangle", "vdotgradvx", "vdotgradvy", "vdotgradvz", "vdotvdotgradv",
+"rhodivv", "absvdotgradv", "vdotvdotgradvcos", "vdotvdotgradvangle"]
+
+field_functions = [drhodx, drhody, drhodz, absv, dvxdx, dvxdy, dvxdz,
+dvydx, dvydy, dvydz, dvzdx, dvzdy, dvzdz, divv, absgradrho, vdotgradrho,
+vdotgradrhocos, vdotgradrhoangle, vdotgradvx, vdotgradvy, vdotgradvz,
+vdotvdotgradv, rhodivv, absvdotgradv, vdotvdotgradvcos, vdotvdotgradvangle]
+
+bools = [False]*len(possible_fields)
+
+dependencies = {
+"divv": {"dvxdx", "dvydy", "dvzdz"},
+"absgradrho": {"drhodx", "drhody", "drhodz"},
+"vdotgradrho": {"drhodx", "drhody", "drhodz"},
+"vdotgradrhocos": {"vdotgradrho", "absv", "absgradrho"},
+"vdotgradrhoangle": {"vdotgradrhocos"},
+"vdotgradvx": {"dvxdx", "dvxdy", "dvxdz"},
+"vdotgradvy": {"dvydx", "dvydy", "dvydz"},
+"vdotgradvz": {"dvzdx", "dvzdy", "dvzdz"},
+"vdotvdotgradv": {"vdotgradvx", "vdotgradvy", "vdotgradvz"},
+"rhodivv": {"divv"},
+"absvdotgradv": {"vdotgradvx", "vdotgradvy", "vdotgradvz"},
+"vdotvdotgradvcos": {"vdotvdotgradv", "absv", "absvdotgradv"},
+"vdotvdotgradvangle": {"vdotvdotgradvcos"}
+}
+
+is_added = {}
+task_to_field = {}
+
+for i in range(len(possible_fields)):
+	is_added[possible_fields[i]] = bools[i]
+	task_to_field[possible_fields[i]] = field_functions[i]
+
+default_validators = [yt.ValidateGridType()]
+density_validators = [yt.ValidateSpatial(1,['density'])]
+vx_validators = [yt.ValidateSpatial(1,['x-velocity'])]
+vy_validators = [yt.ValidateSpatial(1,['y-velocity'])]
+vz_validators = [yt.ValidateSpatial(1,['z-velocity'])]
+
+validators = {"drhodx": density_validators, "drhody": density_validators, "drhodz": density_validators,
+"dvxdx": vx_validators, "dvxdy": vx_validators, "dvxdz": vx_validators,
+"dvydx": vy_validators, "dvydy": vy_validators, "dvydz": vy_validators,
+"dvzdx": vz_validators, "dvzdy": vz_validators, "dvzdz": vz_validators,
+"divv": default_validators, "vdotgradvx": default_validators, "vdotgradvy": default_validators,
+"vdotgradvz": default_validators, "vdotvdotgradv": default_validators,
+"absv": default_validators, "absgradrho": default_validators, "vdotgradrho": default_validators,
+"vdotgradrhocos": default_validators, "vdotgradrhoangle": default_validators, "rhodivv": default_validators,
+"rhodivv": default_validators, "absvdotgradv": default_validators,
+"vdotvdotgradvcos": default_validators, "vdotvdotgradvangle": default_validators}
+
+units = {"drhodx": drho_units, "drhody": drho_units, "drhodz": drho_units,
+"dvxdx": dv_units, "dvxdy": dv_units, "dvxdz": dv_units,
+"dvydx": dv_units,"dvydy": dv_units,"dvydz": dv_units,
+"dvzdx": dv_units,"dvzdy": dv_units,"dvzdz": dv_units,
+"absv": "code_length/code_time", "divv": dv_units,
+"absgradrho": drho_units, "vdotgradrho": "code_mass/(code_length**3*code_time)",
+"divv": dv_units, "vdotgradvx": a_units, "vdotgradvy": a_units, "vdotgradvz": a_units,
+"vdotgradrhocos": "dimensionless", "vdotgradrhoangle": "dimensionless",
+"vdotvdotgradv": "code_length**2/code_time**3", "rhodivv": "code_mass/(code_length**3*code_time)",
+"rhodivv": "code_mass/(code_length**3*code_time)", "absvdotgradv": a_units,
+"vdotvdotgradvcos": "dimensionless", "vdotvdotgradvangle": "dimensionless"}
+
+def add_fields(ds, task):
+	if dependencies.get(task) != None:
+		for field in dependencies[task]:
+			add_fields(ds, field)
+		ds.add_field(task, task_to_field[task], take_log = False, validators = validators[task], sampling_type = 'cell', units = units[task])
+	else:
+		if is_added.get(task) == False:
+			ds.add_field(task, task_to_field[task], take_log = False, validators = validators[task], sampling_type = 'cell', units = units[task])
 
 def load(ds, tasks):
-	needs_densitydx = False
-	needs_densitydy = False
-	needs_densitydz = False
-
-	needs_dBxdx = False; needs_dBxdy = False; needs_dBxdz = False;
-	needs_dBydx = False; needs_dBydy = False; needs_dBydz = False;
-	needs_dBzdx = False; needs_dBzdy = False; needs_dBzdz = False;
-
-	needs_dvxdx = False; needs_dvxdy = False; needs_dvxdz = False;
-	needs_dvydx = False; needs_dvydy = False; needs_dvydz = False;
-	needs_dvzdx = False; needs_dvzdy = False; needs_dvzdz = False;
-
 	for task in tasks:
-		field_name = taskdictstr[task]
-		if needs_densitydx == False and task in density_dx_requirement_set:
-			ds.add_field("drhodx", drhodx, take_log = False, validators = density_validators, sampling_type = 'cell', units = drho_units)
-			print("field %s needs also field drho/dx, adding ..."%field_name)
-			needs_densitydx = True
-		if needs_densitydy == False and task in density_dy_requirement_set:
-			ds.add_field("drhody", drhodx, take_log = False, validators = density_validators, sampling_type = 'cell', units = drho_units)
-			print("field %s needs also field drho/dy, adding ..."%field_name)
-			needs_densitydy = True
-		if needs_densitydz == False and task in density_dy_requirement_set:
-			ds.add_field("drhodz", drhodx, take_log = False, validators = density_validators, sampling_type = 'cell', units = drho_units)
-			print("field %s needs also field drho/dz, adding ..."%field_name)
-			needs_densitydz = True
-
-		if needs_dvxdx == False and task in dvxdx_requirement_set:
-			ds.add_field("dvxdx", dvxdx, take_log = False, validators = vx_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvx/dx, adding ..."%field_name)
-			needs_dvxdx = True
-		if needs_dvxdy == False and task in dvxdy_requirement_set:
-			ds.add_field("dvxdy", dvxdy, take_log = False, validators = vx_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvx/dy, adding ..."%field_name)
-			needs_dvxdy = True
-		if needs_dvxdz == False and task in dvxdz_requirement_set:
-			ds.add_field("dvxdz", dvxdz, take_log = False, validators = vx_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvx/dz, adding ..."%field_name)
-			needs_dvxdz = True
-
-		if needs_dvydx == False and task in dvydx_requirement_set:
-			ds.add_field("dvydx", dvydx, take_log = False, validators = vy_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvy/dx, adding ..."%field_name)
-			needs_dvydx = True
-		if needs_dvydy == False and task in dvydy_requirement_set:
-			ds.add_field("dvydy", dvydy, take_log = False, validators = vy_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvy/dy, adding ..."%field_name)
-			needs_dvydy = True
-		if needs_dvydz == False and task in dvydz_requirement_set:
-			ds.add_field("dvydz", dvydz, take_log = False, validators = vy_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvy/dz, adding ..."%field_name)
-			needs_dvydz = True
-
-		if needs_dvzdx == False and task in dvzdx_requirement_set:
-			ds.add_field("dvzdx", dvzdx, take_log = False, validators = vz_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvz/dx, adding ..."%field_name)
-			needs_dvzdx = True
-		if needs_dvzdy == False and task in dvzdy_requirement_set:
-			ds.add_field("dvzdy", dvzdy, take_log = False, validators = vz_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvz/dy, adding ..."%field_name)
-			needs_dvzdy = True
-		if needs_dvzdz == False and task in dvzdz_requirement_set:
-			ds.add_field("dvzdz", dvzdz, take_log = False, validators = vz_validators, sampling_type = 'cell', units = dv_units)
-			print("field %s needs also field dvz/dz, adding ..."%field_name)
-			needs_dvzdz = True
-
-		if task in taskdictfun:
-			field_function = taskdictfun[task]; field_units = taskdictuni[task]
-			print("adding field %s, units: %s"%(field_name, field_units))
-			ds.add_field(field_name, field_function, take_log = False, validators = [yt.ValidateGridType()], sampling_type = 'cell', units=field_units)
+		if dependencies.get(task) == None:
+			if task_to_field.get(task) != None and is_added.get(task) == False:
+				ds.add_field(task, task_to_field[task], take_log = False, validators = validators[task], sampling_type = 'cell', units = units[task])
 		else:
-			print("field %s exists"%field_name)
+			add_fields(ds, task)
