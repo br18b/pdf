2e9e0bdc47a51fe12897ef8ff4ceef2bda667a14
brano <br18b@my.fsu.edu>
2020-03-17 14:09:28 -0400

    main script

--- pdf.py
+++ pdf.py
@@ -15,9 +15,6 @@ path, filenames, frames, tasks, scales, dxs = load_params("input.txt")
 if not os.path.exists('%s/pdf'%path):
 	os.makedirs('%s/pdf'%path)
 
-taskdictstr = {"vx": "x-velocity", "vy": "y-velocity", "vz": "z-velocity",
-"|gradrho|": "absgradrho"}
-
 for frame in frames:
 	print("loading %s/%s%04d/%s%04d ..."%(path,filenames[0],frame,filenames[1],frame))
 	ds = yt.load('%s/%s%04d/%s%04d'%(path,filenames[0],frame,filenames[1],frame))
@@ -27,24 +24,23 @@ for frame in frames:
 		task = tasks[i]
 		scale = scales[i]
 		dx = dxs[i]
-		if task in taskdictstr:
-			field_name = taskdictstr[task]
-		else:
-			field_name = task
+		field_name = task
 
 		minval = region[field_name].min().v
 		maxval = region[field_name].max().v
 		print("field: %s, min: %f, max: %f"%(field_name, minval, maxval))
 		if scale == "lin":
 			N = max(int((maxval-minval)/dx), 1)
-			field_bins = numpy.linspace(minval, maxval, N)
+			field_bins = numpy.linspace(0.999*minval, 1.001*maxval, N)
 		elif scale == "log":
 			N = 1000
-			field_bins = numpy.logspace(numpy.log10(0.8*minval), numpy.log10(1.2*maxval), N)
+			field_bins = numpy.logspace(numpy.log10(0.999*minval), numpy.log10(1.001*maxval), N)
 		elif scale == "symlog":
 			N = 1000
-			field_bins = numpy.logspace(-5, numpy.log10(1.2*max(abs(minval), abs(maxval))), N)
-			field_bins = (-field_bins)[::-1] + field_bins
+			absmax = max(abs(minval), abs(maxval))
+			N2 = max(100, int(N*dx/absmax))
+			field_bins = numpy.logspace(numpy.log10(1.001*dx), numpy.log10(1.001*absmax), N)
+			field_bins = numpy.concatenate(((-field_bins)[::-1], numpy.linspace(-dx, dx, N2), field_bins))
 		bins = {field_name: field_bins}
 		profile = yt.create_profile(region, bin_fields = [field_name], fields = ['cell_volume'], weight_field = None, fractional = True, override_bins = bins)
 		P = numpy.column_stack((bin_center(field_bins),profile['cell_volume']/bin_size(field_bins)))
